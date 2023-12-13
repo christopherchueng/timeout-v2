4d3b33ec7845e59d91a155f7116be4632f91fb19
Christopher Chueng <christopherchueng@gmail.com>
2023-12-13 14:29:36 -0500

    Remove console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ export default async function Home() {
   const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
-  console.log("here is sesion", session);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
