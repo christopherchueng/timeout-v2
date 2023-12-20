8a50a00a90612238bf1c7c9412d2d02522c347fb
Christopher Chueng <christopherchueng@gmail.com>
2023-12-19 20:04:37 -0500

    Separate SVG files into reusable components

---
+++ src/app/_components/UI/Plus.tsx
@@ -0,0 +1,19 @@
+const Plus = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    data-slot="icon"
+    className="h-5 w-5"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 4.5v15m7.5-7.5h-15"
+    />
+  </svg>
+);
+
+export default Plus;
--- src/app/_components/UI/index.tsx
+++ src/app/_components/UI/index.tsx
@@ -1,4 +1,7 @@
+import Chevron from "./Chevron";
+import CreateAlarmlist from "./CreateAlarmlist";
 import CreateButton from "./CreateButton";
+import Plus from "./Plus";
 import Switch from "./Switch";
 
-export { CreateButton, Switch };
+export { Chevron, CreateAlarmlist, CreateButton, Plus, Switch };
