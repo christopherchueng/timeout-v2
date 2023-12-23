72ef38a7678d130bc37493772ada24f1c33957a3
Christopher Chueng <christopherchueng@gmail.com>
2023-12-22 22:41:58 -0500

    Adjust toggle spacing on alarms

--- src/app/_components/Alarm/index.tsx
+++ src/app/_components/Alarm/index.tsx
@@ -5,6 +5,7 @@ import { Switch } from "../UI";
 import { api } from "@/trpc/react";
 import type { RouterOutputs } from "@/trpc/shared";
 import AlarmIcon from "../UI/AlarmIcon";
+import Ellipsis from "../UI/Ellipsis";
 
 type Alarm = RouterOutputs["alarm"]["getAllByAlarmlistId"][number];
 
@@ -74,8 +75,8 @@ const Alarm = ({ alarm, handleAlarmlistToggle }: AlarmProps) => {
 
   return (
     <ul>
-      <li className="flex flex-row items-center justify-between rounded-xl border border-transparent py-1 pl-4 pr-2 transition duration-200 hover:bg-gray-200">
-        <div className="flex items-center gap-2">
+      <li className="group relative flex h-10 flex-row items-center justify-between rounded-lg border border-transparent py-1 pl-4 pr-2 transition duration-200 hover:bg-gray-200">
+        <div className="absolute flex w-3/4 items-center gap-2 group-hover:w-[73%]">
           <AlarmIcon isOn={alarm.isOn} />
           <div
             className={clsx("flex flex-col transition", {
@@ -91,13 +92,18 @@ const Alarm = ({ alarm, handleAlarmlistToggle }: AlarmProps) => {
             <div className="text-xs">{alarm.name}</div>
           </div>
         </div>
-        <Switch
-          id={alarm.id}
-          checked={alarm.isOn}
-          onChange={(e) => {
-            mutate({ id: alarm.id, isOn: e.currentTarget.checked });
-          }}
-        />
+        <div className="absolute right-1 inline-flex w-auto gap-1.5">
+          <div className="relative">
+            <Ellipsis />
+          </div>
+          <Switch
+            id={alarm.id}
+            checked={alarm.isOn}
+            onChange={(e) => {
+              mutate({ id: alarm.id, isOn: e.currentTarget.checked });
+            }}
+          />
+        </div>
       </li>
     </ul>
   );
--- src/app/_components/Alarmlist/Settings.tsx
+++ src/app/_components/Alarmlist/Settings.tsx
@@ -50,9 +50,6 @@ const Settings = ({
 
   return (
     <div className="absolute right-0 top-6 z-10 flex w-28 animate-dilate flex-col rounded border bg-white p-2 sm:left-0 ">
-      {/* <div className="cursor-pointer rounded-md px-2 py-2 hover:bg-gray-200">
-        <span>Edit</span>
-      </div> */}
       <div
         onClick={handleDeleteAlarmlistModal}
         className="flex cursor-pointer items-center gap-1.5 rounded-md px-2 py-2 hover:bg-gray-200"
