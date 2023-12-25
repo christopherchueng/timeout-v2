2fd2fc1fd553638514f667049a1575d4ebda4d17
Christopher Chueng <christopherchueng@gmail.com>
2023-12-24 23:35:58 -0500

    Create settings wrapper for overlay

---
+++ src/app/_components/Alarmlist/SettingsWrapper.tsx
@@ -0,0 +1,57 @@
+import React, {
+  useEffect,
+  type ComponentPropsWithoutRef,
+  forwardRef,
+} from "react";
+import Portal from "../Portal";
+
+interface SettingsWrapperProps extends ComponentPropsWithoutRef<"button"> {
+  children: React.ReactNode;
+  ref: HTMLInputElement | null;
+  isOpen: boolean;
+  cursorPosition: {
+    x: number;
+    y: number;
+  };
+  handleClose: ({ target }: MouseEvent) => void;
+}
+
+const SettingsWrapper = forwardRef<HTMLInputElement, SettingsWrapperProps>(
+  (
+    { children, isOpen, handleClose, cursorPosition }: SettingsWrapperProps,
+    ref,
+  ) => {
+    useEffect(() => {
+      const handleClickedOutside = (e: MouseEvent) => {
+        handleClose(e);
+      };
+
+      document.addEventListener("click", handleClickedOutside);
+
+      return () => {
+        document.removeEventListener("click", handleClickedOutside);
+      };
+    }, [handleClose]);
+
+    if (!isOpen) return null;
+
+    return (
+      <Portal wrapperId="Settings_Overlay">
+        <div className="absolute inset-0 z-10 min-h-full w-full bg-transparent">
+          <div
+            ref={ref}
+            style={{
+              left: `${cursorPosition.x - 5}px`,
+              top: `${cursorPosition.y + 10}px`,
+            }}
+            className="absolute left-0 z-20 rounded border bg-white"
+          >
+            {children}
+          </div>
+        </div>
+      </Portal>
+    );
+  },
+);
+
+export default SettingsWrapper;
