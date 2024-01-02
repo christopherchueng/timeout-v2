2bca3a7f3202712a65119c8bb347d0f13ca8608a
Christopher Chueng <christopherchueng@gmail.com>
2024-01-02 18:21:55 -0500

    Add background color to tooltip

--- src/app/_components/UI/Tooltip.tsx
+++ src/app/_components/UI/Tooltip.tsx
@@ -7,7 +7,7 @@ const Tooltip = ({ text, children }: TooltipProps) => {
   return (
     <div className="relative inline-block">
       {children}
-      <span className="absolute -bottom-16 -left-4 z-50 animate-dilate whitespace-nowrap rounded border px-2 py-1.5 text-xs transition duration-75">
+      <span className="absolute -bottom-16 -left-4 z-50 animate-dilate whitespace-nowrap rounded border bg-white px-2 py-1.5 text-xs transition duration-75">
         {text}
       </span>
     </div>
