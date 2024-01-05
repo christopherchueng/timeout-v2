f9f6d48900aed4623e3a91abb5f1c2c9946121de
Christopher Chueng <christopherchueng@gmail.com>
2024-01-04 20:45:02 -0500

    Create useSettingsActions custom hook

---
+++ src/hooks/useSettingsActions.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react";
+
+type SettingStatus = {
+  isOpen: boolean;
+  isHovering: boolean;
+};
+
+const useSettingsActions = () => {
+  const [settingsTab, setSettingsTab] = useState<SettingStatus>({
+    isHovering: false,
+    isOpen: false,
+  });
+
+  const closeSettings = useCallback(() => {
+    setSettingsTab((prev) => ({
+      ...prev,
+      isOpen: false,
+    }));
+  }, []);
+
+  const openSettings = useCallback(() => {
+    setSettingsTab((prev) => ({
+      ...prev,
+      isOpen: true,
+    }));
+  }, []);
+
+  return {
+    settingsTab,
+    closeSettings,
+    openSettings,
+    setSettingsTab,
+  };
+};
+
+export default useSettingsActions;
