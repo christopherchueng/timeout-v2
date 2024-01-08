db92c819e8e424b59a551e38cfe91c05a023452c
Christopher Chueng <christopherchueng@gmail.com>
2024-01-08 12:08:28 -0500

    Create weekdays object for mapping

---
+++ src/app/_components/Alarm/constants.ts
@@ -0,0 +1,31 @@
+export const weekdaysData: { [key: string]: { value: number; short: string } } =
+  {
+    Sun: {
+      value: 0,
+      short: "S",
+    },
+    Mon: {
+      value: 1,
+      short: "M",
+    },
+    Tue: {
+      value: 2,
+      short: "T",
+    },
+    Wed: {
+      value: 3,
+      short: "W",
+    },
+    Thu: {
+      value: 4,
+      short: "T",
+    },
+    Fri: {
+      value: 5,
+      short: "F",
+    },
+    Sat: {
+      value: 6,
+      short: "S",
+    },
+  } as const;
