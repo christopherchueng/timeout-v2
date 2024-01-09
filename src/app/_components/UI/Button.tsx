e105cbd7ff87437a7fea4b038b58134f94f2912d
Christopher Chueng <christopherchueng@gmail.com>
2024-01-09 16:49:18 -0500

    Add more button ui styling

--- src/app/_components/UI/Button.tsx
+++ src/app/_components/UI/Button.tsx
@@ -1,16 +1,26 @@
+import clsx from "clsx";
 import { type ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  color?: "default" | "primary";
 }
 
-const Button = ({ children, ...props }: ButtonProps) => (
-  <button
-    {...props}
-    className="flex select-none items-center justify-center whitespace-nowrap rounded border-slate-900 bg-slate-900 px-3 py-1.5 text-xs text-white transition hover:bg-slate-700 active:bg-slate-800 disabled:cursor-not-allowed disabled:bg-gray-300"
-  >
-    {children}
-  </button>
-);
+const Button = ({ children, color = "primary", ...props }: ButtonProps) => {
+  const buttonClassNames = clsx(
+    "flex select-none items-center justify-center whitespace-nowrap rounded px-3 py-1.5 text-xs text-white transition disabled:cursor-not-allowed disabled:bg-gray-300",
+    {
+      "border-slate-900 bg-slate-900 hover:bg-slate-700 active:bg-slate-800":
+        color === "primary",
+      "border-gray-400 bg-gray-400 hover:opacity-80 active:opacity-90":
+        color === "default",
+    },
+  );
+  return (
+    <button {...props} className={buttonClassNames}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
