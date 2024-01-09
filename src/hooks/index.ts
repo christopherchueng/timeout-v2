1b3b6b925d2775ab974174e143c5698c1ac9ff58
Christopher Chueng <christopherchueng@gmail.com>
2024-01-09 11:51:38 -0500

    Create useTriggerAlarm hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,13 @@
 import useCursorPosition from "./useCursorPosition";
 import useSettingsActions from "./useSettingsActions";
 import useSnoozeCountdown from "./useSnoozeCountdown";
+import useTriggerAlarm from "./useTriggerAlarm";
 import useWindowDimensions from "./useWindowDimensions";
 
 export {
   useCursorPosition,
   useSettingsActions,
   useSnoozeCountdown,
+  useTriggerAlarm,
   useWindowDimensions,
 };
---
+++ src/hooks/useTriggerAlarm.ts
@@ -0,0 +1,33 @@
+import React, { useMemo } from "react";
+import type { Alarm, Value } from "@/types";
+import dayjs from "dayjs";
+import { useTimeContext } from "@/context/Time";
+import { weekdaysData } from "@/utils/constants";
+
+const useTriggerAlarm = (alarm: Alarm) => {
+  const { currentDate } = useTimeContext();
+
+  const isAlarmTriggered = useMemo(() => {
+    const currentHour = dayjs().get("hour");
+    const alarmMatchesTime =
+      alarm.isOn &&
+      alarm.hour === (currentHour % 12 || 12) &&
+      alarm.minutes === dayjs().get("minute") &&
+      dayjs().get("second") === 0 &&
+      alarm.meridiem === (currentHour >= 12 && currentHour <= 23 ? "PM" : "AM");
+
+    if (!alarm.repeat) {
+      return alarmMatchesTime;
+    }
+
+    const repeatDays = alarm.repeat.split(",").map((number) => {
+      return weekdaysData[number as Value]!.value;
+    });
+
+    return alarmMatchesTime && repeatDays.includes(currentDate.get("day") + 1);
+  }, [currentDate]);
+
+  return { isAlarmTriggered };
+};
+
+export default useTriggerAlarm;
