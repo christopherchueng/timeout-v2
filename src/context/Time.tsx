e171c6d439abe4b2b40db0a8250031618f44c668
Christopher Chueng <christopherchueng@gmail.com>
2024-01-11 21:34:11 -0500

    Update Time Context logic based on 12 hr mode

--- src/context/Time.tsx
+++ src/context/Time.tsx
@@ -2,6 +2,7 @@
 
 import { createContext, useContext, useState, useEffect } from "react";
 import dayjs, { type Dayjs } from "dayjs";
+import { api } from "@/trpc/react";
 
 type TimeContextType = {
   currentDate: Dayjs;
@@ -24,7 +25,7 @@ type Part = {
   hour: string | number;
   minute: string | number;
   second: string | number;
-  meridiem: string;
+  meridiem: string | undefined;
   day: string;
 };
 
@@ -33,23 +34,29 @@ type TimeProps = {
 };
 
 const TimeProvider = ({ children }: TimeProps) => {
+  const { data: preferences } = api.preference.get.useQuery();
+
   const currentDate = dayjs();
   const [parts, setParts] = useState<Part>({
     hour: "",
     minute: "",
     second: "",
-    meridiem: "",
+    meridiem: undefined,
     day: "",
   });
 
   useEffect(() => {
+    if (!preferences) return;
+
     const timeInterval = setInterval(
       () =>
         setParts({
-          hour: dayjs(currentDate).format("h"),
+          hour: dayjs(currentDate).format(preferences.use12Hour ? "h" : "H"),
           minute: dayjs(currentDate).format("mm"),
           second: dayjs(currentDate).format("s"),
-          meridiem: dayjs(currentDate).format("A"),
+          meridiem: preferences.use12Hour
+            ? dayjs(currentDate).format("A")
+            : undefined,
           day: dayjs(currentDate).format("ddd"),
         }),
       0,
