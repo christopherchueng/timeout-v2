d88013a529674a106eabbec028c3d09332ef2b72
Christopher Chueng <christopherchueng@gmail.com>
2024-01-13 11:27:10 -0500

    Create preferences page

---
+++ src/app/preferences/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Mode from "./Mode";
+import { useSession } from "next-auth/react";
+
+const PreferencesPage = () => {
+  const { data: session } = useSession();
+
+  if (!session) return;
+
+  return (
+    <main className="flex h-full w-full justify-center px-6 text-slate-900 sm:px-0">
+      <div className="flex h-full w-full max-w-2xl flex-col items-center gap-10 pt-18 sm:px-6">
+        <div className="mt-10 w-full">
+          <h1 className="text-xl font-bold">Preferences</h1>
+        </div>
+        <div className="flex w-full flex-row items-center justify-between gap-4">
+          <div className="leading-8">
+            <span className="text-sm font-semibold">Standard time</span>
+            <p className="min-w-8 max-w-prose text-xs text-gray-400">
+              Display time in 12-hour format. Turn off to switch to military
+              time (24-hour format).
+            </p>
+          </div>
+          <Mode session={session} />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default PreferencesPage;
