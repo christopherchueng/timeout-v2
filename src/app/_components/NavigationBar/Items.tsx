6554616c835b2549ed49be9c06f8884adda0a97f
Christopher Chueng <christopherchueng@gmail.com>
2024-01-13 11:28:09 -0500

    Create profile picture component and add to navbar

--- src/app/_components/NavigationBar/Items.tsx
+++ src/app/_components/NavigationBar/Items.tsx
@@ -1,21 +1,20 @@
-import { Signin, Signout } from "../Auth";
+import { Signin } from "../Auth";
 import CreateButton from "./CreateButton";
 import { useSession } from "next-auth/react";
-import Preferences from "./Preferences";
+import ProfilePicture from "./ProfilePicture";
 
 const Items = () => {
   const { data: session } = useSession();
 
   return (
-    <div className="flex w-full select-none justify-end gap-2.5 sm:gap-4">
+    <div className="flex w-full select-none justify-end gap-5 sm:gap-4">
       {session && (
         <>
           <CreateButton />
           <div className="h-5 self-center border-l border-slate-900 text-slate-500" />
-          <Preferences session={session} />
         </>
       )}
-      {session ? <Signout /> : <Signin />}
+      {session ? <ProfilePicture imageUrl={session.user.image!} /> : <Signin />}
     </div>
   );
 };
---
+++ src/app/_components/NavigationBar/ProfilePicture.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signOut } from "next-auth/react";
+import { Cog, ExitIcon } from "../UI";
+import Link from "next/link";
+
+const ProfilePicture = ({ imageUrl }: { imageUrl: string }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (!isMenuOpen) return;
+
+    const closeTab = () => {
+      setIsMenuOpen(false);
+    };
+
+    document.addEventListener("click", closeTab);
+
+    return () => {
+      document.removeEventListener("click", closeTab);
+    };
+  }, [isMenuOpen]);
+
+  return (
+    <div>
+      <button onClick={() => setIsMenuOpen((prev) => !prev)}>
+        <Image
+          className="rounded-full"
+          src={imageUrl}
+          alt="Profile picture"
+          height={40}
+          width={40}
+        />
+      </button>
+      {isMenuOpen && (
+        <div className="relative animate-dilate transition-all">
+          <div className="absolute right-0 top-1.5 z-50 flex h-fit w-36 flex-col items-start rounded-md border bg-white p-2 shadow-lg">
+            <Link
+              href="/preferences"
+              className="inline-flex w-full cursor-pointer gap-1.5 rounded-md p-2 hover:bg-gray-200"
+            >
+              <Cog />
+              <span>Preferences</span>
+            </Link>
+            <button
+              onClick={() => signOut({ callbackUrl: "/" })}
+              className="inline-flex w-full cursor-pointer gap-1.5 rounded-md p-2 text-start hover:bg-gray-200"
+            >
+              <ExitIcon />
+              Sign out
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePicture;
