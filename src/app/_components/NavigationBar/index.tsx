3c90f59aa27e2dd3434e9b90aef900a05a6c2ef8
Christopher Chueng <christopherchueng@gmail.com>
2024-01-14 18:05:00 -0500

    Refactor Navigation bar

--- src/app/_components/NavigationBar/index.tsx
+++ src/app/_components/NavigationBar/index.tsx
@@ -4,20 +4,16 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Items from "./Items";
 
-const Logo = () => {
-  const { data: session } = useSession();
-
-  return (
-    <div className="w-full">
-      <Link href={session ? "/dashboard" : "/"}>Home</Link>
-    </div>
-  );
-};
-
 const NavigationBar = () => {
+  const { status } = useSession();
+
+  if (status === "loading") return;
+
   return (
     <nav className="absolute z-10 flex w-full items-center justify-center bg-white px-6 py-4 text-xs sm:px-8">
-      <Logo />
+      <div className="w-full">
+        <Link href={status === "authenticated" ? "/dashboard" : "/"}>Home</Link>
+      </div>
       <Items />
     </nav>
   );
