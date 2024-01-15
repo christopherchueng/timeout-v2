2d5e418f9e8119a67363ee3542ac896f4073ec9e
Christopher Chueng <christopherchueng@gmail.com>
2024-01-15 17:44:20 -0500

    Style profile picture for navbar consistency

--- src/app/_components/NavigationBar/ProfilePicture.tsx
+++ src/app/_components/NavigationBar/ProfilePicture.tsx
@@ -22,19 +22,22 @@ const ProfilePicture = ({ imageUrl }: { imageUrl: string }) => {
   }, [isMenuOpen]);
 
   return (
-    <div>
-      <button onClick={() => setIsMenuOpen((prev) => !prev)}>
+    <div className="flex items-center">
+      <button
+        className="rounded-full border border-slate-900"
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+      >
         <Image
           className="rounded-full"
           src={imageUrl}
           alt="Profile picture"
-          height={40}
-          width={40}
+          height={28}
+          width={28}
         />
       </button>
       {isMenuOpen && (
         <div className="relative animate-dilate transition-all">
-          <div className="absolute right-0 top-1.5 z-50 flex h-fit w-36 flex-col items-start rounded-md border bg-white p-2 shadow-lg">
+          <div className="absolute right-0 top-5 z-50 flex h-fit w-36 flex-col items-start rounded-md border bg-white p-2 shadow-lg">
             <Link
               href="/preferences"
               className="inline-flex w-full cursor-pointer gap-1.5 rounded-md p-2 hover:bg-gray-200"
