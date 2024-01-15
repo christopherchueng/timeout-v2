5dd4cc5475d04f3198fb7ef8ee6ec0d1228168c7
Christopher Chueng <christopherchueng@gmail.com>
2024-01-15 17:42:40 -0500

    Show different logo based on window size

--- src/app/_components/NavigationBar/index.tsx
+++ src/app/_components/NavigationBar/index.tsx
@@ -3,6 +3,7 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Items from "./Items";
+import { Logo, NameLogo } from "../UI";
 
 const NavigationBar = () => {
   const { status } = useSession();
@@ -10,7 +11,16 @@ const NavigationBar = () => {
   return (
     <nav className="absolute z-10 flex w-full items-center justify-center bg-white px-6 py-4 text-xs sm:px-8">
       <div className="w-full">
-        <Link href={status === "authenticated" ? "/dashboard" : "/"}>Home</Link>
+        <div className="hidden w-fit sm:block">
+          <Link href={status === "authenticated" ? "/dashboard" : "/"}>
+            <NameLogo className="h-8 w-24" />
+          </Link>
+        </div>
+        <div className="w-fit sm:hidden">
+          <Link href={status === "authenticated" ? "/dashboard" : "/"}>
+            <Logo className="h-7 w-7" />
+          </Link>
+        </div>
       </div>
       <Items />
     </nav>
