e02d80d55f49c5f68d06ccb148ad2e1904ac0589
Christopher Chueng <christopherchueng@gmail.com>
2024-01-15 19:21:19 -0500

    Fix spacing on sign in screen

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,14 +3,12 @@ import { NameLogo } from "../_components/UI";
 
 const SigninPage = () => {
   return (
-    <div className="relative flex h-full flex-col items-center justify-center pt-18">
-      <div className="absolute top-40 text-5xl font-semibold tracking-tight">
+    <div className="relative flex h-full flex-col items-center justify-center">
+      <div className="absolute top-40 flex flex-col items-center justify-center space-y-28">
         <NameLogo />
-      </div>
-      <div className="flex h-36 w-96 flex-col items-center justify-center gap-12 rounded-lg border py-6 shadow-xl">
-        {/* <h1 className="text-2xl font-semibold">Sign in</h1>
-        <hr className="w-64" /> */}
-        <Signin />
+        <div className="flex h-36 w-96 items-center justify-center rounded-lg border py-6 shadow-xl">
+          <Signin />
+        </div>
       </div>
     </div>
   );
