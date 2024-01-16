eaa0f6e817251d7360a768bfe20eece736656519
Christopher Chueng <christopherchueng@gmail.com>
2024-01-15 20:05:39 -0500

    Refactor navbar items and create loading state

--- src/app/_components/NavigationBar/Items.tsx
+++ src/app/_components/NavigationBar/Items.tsx
@@ -1,10 +1,29 @@
-import { Signin } from "../Auth";
 import CreateButton from "./CreateButton";
 import { useSession } from "next-auth/react";
 import ProfilePicture from "./ProfilePicture";
 
+const Loading = () => (
+  <div className="h-6 w-16 animate-pulse rounded bg-gray-200 px-3 py-1.5 transition" />
+);
+
 const Items = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  const rightItem = () => {
+    if (status === "loading") return <Loading />;
+
+    if (status === "authenticated")
+      return <ProfilePicture imageUrl={session?.user.image!} />;
+
+    return (
+      <a
+        className="flex select-none items-center justify-center whitespace-nowrap rounded border-slate-900 bg-slate-900 px-3 py-1.5 text-xs text-white transition hover:bg-slate-700 active:bg-slate-800 disabled:cursor-not-allowed disabled:bg-gray-300"
+        href="/signin"
+      >
+        Sign in
+      </a>
+    );
+  };
 
   return (
     <div className="flex w-full select-none justify-end gap-5 sm:gap-4">
@@ -14,16 +33,7 @@ const Items = () => {
           <div className="h-5 self-center border-l border-slate-900 text-slate-500" />
         </>
       )}
-      {session ? (
-        <ProfilePicture imageUrl={session.user.image!} />
-      ) : (
-        <a
-          className="flex select-none items-center justify-center whitespace-nowrap rounded border-slate-900 bg-slate-900 px-3 py-1.5 text-xs text-white transition hover:bg-slate-700 active:bg-slate-800 disabled:cursor-not-allowed disabled:bg-gray-300"
-          href="/signin"
-        >
-          Sign in
-        </a>
-      )}
+      {rightItem()}
     </div>
   );
 };
