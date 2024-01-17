ac7918f4e5a0817ae25f0053c25d616838a8d8d6
Christopher Chueng <christopherchueng@gmail.com>
2024-01-17 18:15:41 -0500

    styling: add more left padding to alarm for drag handle

--- src/app/_components/Alarm/index.tsx
+++ src/app/_components/Alarm/index.tsx
@@ -289,7 +289,7 @@ const Alarm = ({ alarm, handleAlarmlistToggle }: AlarmProps) => {
       onMouseLeave={() =>
         setSettingsTab((prev) => ({ ...prev, isHovering: false }))
       }
-      className="group relative mt-0.5 flex h-10 flex-row items-center justify-between rounded-lg border border-transparent py-1 pl-4 pr-2 transition duration-200 hover:bg-gray-200"
+      className="pl-5.5 group relative mt-0.5 flex h-10 flex-row items-center justify-between rounded-lg border border-transparent py-1 pr-2 transition duration-200 hover:bg-gray-200"
     >
       <div className="absolute flex w-3/4 items-center gap-2 group-hover:w-[73%]">
         <AlarmIcon isOn={alarm.isOn} />
