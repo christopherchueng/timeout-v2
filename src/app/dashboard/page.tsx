6bea7041c8d0095c873bf61703ffbaef788d5b42
Christopher Chueng <christopherchueng@gmail.com>
2024-01-18 17:34:50 -0500

    Style: include dark mode to dashboard bg

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -19,9 +19,9 @@ const Dashboard = () => {
   if (status === "loading") return <></>;
 
   return (
-    <main className="flex h-full w-full flex-row items-center justify-center">
+    <main className="flex h-full w-full flex-row items-center justify-center dark:bg-zinc-900">
       <Sidebar />
-      <div className="z-0 hidden h-full w-full items-center text-7xl sm:flex sm:justify-center md:text-9xl">
+      <div className="z-0 hidden h-full w-full items-center text-7xl dark:bg-zinc-900 sm:flex sm:justify-center md:text-9xl">
         <Clock size="lg" />
       </div>
     </main>
