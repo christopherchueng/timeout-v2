106b202f4af1fbb836b5e74f6ab3f3327c89a8a9
Christopher Chueng <christopherchueng@gmail.com>
2024-01-18 17:35:07 -0500

    Style: include dark mode to splash page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ export default function Home() {
   if (status === "authenticated") redirect("/dashboard");
 
   return (
-    <main className="flex min-h-screen w-full select-none flex-col items-center justify-center gap-6 text-7xl md:text-9xl">
+    <main className="flex min-h-screen w-full select-none flex-col items-center justify-center gap-6 text-7xl dark:bg-zinc-900 md:text-9xl">
       {!hour && <Loading size="lg" />}
       {hour && (
         <>
-          <div className="flex justify-center gap-1">
+          <div className="flex justify-center gap-1 dark:text-white/70">
             <span>{hour}</span>
             <span className="w-20 animate-blink text-center transition">:</span>
             <span>{minute}</span>&nbsp;
@@ -35,7 +35,9 @@ export default function Home() {
                 key={abbr}
                 className={clsx(
                   "text-xs uppercase",
-                  abbr === day ? "text-slate-900" : "text-gray-200",
+                  abbr === day
+                    ? "text-slate-900 dark:text-white/70"
+                    : "text-gray-200 dark:text-gray-400/40",
                 )}
               >
                 {abbr}
