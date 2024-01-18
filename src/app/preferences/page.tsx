b4a9e797ccecacedd00e82d2558416c2a12272ba
Christopher Chueng <christopherchueng@gmail.com>
2024-01-18 17:35:41 -0500

    Add dark mode toggle to preferences

--- src/app/preferences/page.tsx
+++ src/app/preferences/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { getServerAuthSession } from "@/server/auth";
-import Mode from "./Mode";
+import ModeSwitch from "./ModeSwitch";
+import ThemeSwitch from "./ThemeSwitch";
 
 const PreferencesPage = async () => {
   const session = await getServerAuthSession();
@@ -8,7 +9,7 @@ const PreferencesPage = async () => {
   if (!session) return;
 
   return (
-    <main className="relative flex h-full w-full justify-center px-6 text-slate-900 sm:px-0">
+    <main className="relative flex h-full w-full justify-center px-6 text-slate-900 transition dark:bg-zinc-900 dark:text-white/80 sm:px-0">
       <div className="absolute top-16 flex max-w-2xl flex-col items-center gap-10 px-6 pt-6">
         <div className="w-full underline underline-offset-2">
           <Link href="/dashboard" className="text-xs">
@@ -18,15 +19,27 @@ const PreferencesPage = async () => {
         <div className="w-full">
           <h1 className="text-xl font-bold">Preferences</h1>
         </div>
-        <div className="flex w-full flex-col items-center justify-between gap-1.5">
-          <div className="flex w-full justify-between leading-8">
-            <span className="text-sm font-semibold">Standard time</span>
-            <Mode session={session} />
+        <div className="flex w-full flex-col items-center justify-between gap-8">
+          <div className="flex flex-col gap-1.5">
+            <div className="flex w-full justify-between leading-8">
+              <span className="text-sm font-semibold">Standard time</span>
+              <ModeSwitch session={session} />
+            </div>
+            <p className="min-w-8 max-w-prose text-xs text-gray-400">
+              Display time in 12-hour format. Turn off to switch to military
+              time (24-hour format).
+            </p>
           </div>
-          <p className="min-w-8 max-w-prose text-xs text-gray-400">
-            Display time in 12-hour format. Turn off to switch to military time
-            (24-hour format).
-          </p>
+          <div className="flex flex-col gap-1.5">
+            <div className="flex w-full justify-between leading-8">
+              <span className="text-sm font-semibold">Dark mode</span>
+              <ThemeSwitch />
+            </div>
+            <p className="min-w-8 max-w-prose text-xs text-gray-400">
+              Allergic to the sun? SPF should help... or you could stay in the
+              shade in dark mode.
+            </p>
+          </div>
         </div>
       </div>
     </main>
