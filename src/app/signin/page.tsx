8e1c0e15bb29bc533d49afa21194689016f422bd
Christopher Chueng <christopherchueng@gmail.com>
2024-01-18 17:29:00 -0500

    Style: sign in page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -9,10 +9,10 @@ const SigninPage = async () => {
   if (session) redirect("/dashboard");
 
   return (
-    <div className="relative flex h-full flex-col items-center justify-center">
+    <div className="relative flex h-full flex-col items-center justify-center dark:bg-zinc-900">
       <div className="absolute top-40 flex flex-col items-center justify-center space-y-28">
         <NameLogo />
-        <div className="flex h-44 w-96 flex-col items-center justify-center gap-4 rounded-lg border py-6 shadow-xl">
+        <div className="flex h-44 w-96 flex-col items-center justify-center gap-4 rounded-lg border py-6 shadow-xl dark:border-zinc-700">
           <Signin />
           <p className="px-10 text-center text-xs italic text-gray-400">
             An account will be created for you during the sign-in process even
