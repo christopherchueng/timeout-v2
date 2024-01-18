036cbc69def827e14fd15e92da2f3c9be8e3b8ef
Christopher Chueng <christopherchueng@gmail.com>
2024-01-18 17:31:38 -0500

    Style: include dark mode to clock loading skeleton

--- src/app/_components/Clock/loading.tsx
+++ src/app/_components/Clock/loading.tsx
@@ -8,7 +8,7 @@ const Loading = ({ size = "lg" }: ClockProps) => {
   return (
     <div className="flex w-fit flex-row">
       <div
-        className={clsx("animate-pulse bg-gray-200", {
+        className={clsx("animate-pulse bg-gray-200 dark:bg-zinc-700", {
           "h-7 w-16 rounded-md": size === "sm",
           "h-24 w-96 rounded-lg": size === "lg",
         })}
