43ddbcf64eeb2ef61625dabdf5f222d200f6776c
Christopher Chueng <christopherchueng@gmail.com>
2024-01-18 17:34:34 -0500

    Style: include dark mode to switch + add out of the box styling

--- src/app/_components/UI/Switch.tsx
+++ src/app/_components/UI/Switch.tsx
@@ -2,10 +2,13 @@
 
 import { Switch as SwitchComponent } from "@nextui-org/switch";
 import { extendVariants } from "@nextui-org/system";
+import { MoonIcon, SunIcon } from ".";
 
 type SwitchProps = {
   id: string;
+  size?: "xs" | "sm" | "md" | "lg";
   checked: boolean;
+  showIcon?: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
@@ -14,13 +17,75 @@ type SwitchProps = {
 // https://nextui.org/docs/customization/custom-variants
 
 export const MySwitch = extendVariants(SwitchComponent, {
+  slots: {
+    base: "group relative max-w-fit inline-flex items-center justify-start cursor-pointer touch-none tap-highlight-transparent",
+    wrapper: [
+      "px-1",
+      "relative",
+      "inline-flex",
+      "items-center",
+      "justify-start",
+      "flex-shrink-0",
+      "overflow-hidden",
+      "bg-default-200",
+      "rounded-full",
+      // focus ring
+      // ...groupDataFocusVisibleClasses,
+    ],
+    thumb: [
+      "z-10",
+      "flex",
+      "items-center",
+      "justify-center",
+      "bg-white",
+      "shadow-small",
+      "rounded-full",
+      "origin-right",
+    ],
+    startContent: "z-0 absolute left-1.5 text-current",
+    endContent: "z-0 absolute right-1.5 text-default-600",
+    thumbIcon: "text-black",
+    label: "relative text-foreground select-none",
+  },
   variants: {
     color: {
       default: {
         wrapper: [
           "bg-gray-300",
+          "dark:bg-gray-400",
           "group-data-[selected=true]:bg-slate-900",
+          "dark:group-data-[selected=true]:bg-slate-700",
           "group-data-[selected=true]:text-primary-foreground",
+        ],
+      },
+      primary: {
+        wrapper: [
+          "group-data-[selected=true]:bg-primary",
+          "group-data-[selected=true]:text-primary-foreground",
+        ],
+      },
+      secondary: {
+        wrapper: [
+          "group-data-[selected=true]:bg-secondary",
+          "group-data-[selected=true]:text-secondary-foreground",
+        ],
+      },
+      success: {
+        wrapper: [
+          "group-data-[selected=true]:bg-success",
+          "group-data-[selected=true]:text-success-foreground",
+        ],
+      },
+      warning: {
+        wrapper: [
+          "group-data-[selected=true]:bg-warning",
+          "group-data-[selected=true]:text-warning-foreground",
+        ],
+      },
+      danger: {
+        wrapper: [
+          "group-data-[selected=true]:bg-danger",
+          "data-[selected=true]:text-danger-foreground",
         ],
       },
     },
@@ -59,15 +124,97 @@ export const MySwitch = extendVariants(SwitchComponent, {
         startContent: "text-small",
         label: "text-small",
       },
+      sm: {
+        wrapper: "w-10 h-6 mr-2",
+        thumb: [
+          "w-4 h-4 text-tiny",
+          //selected
+          "group-data-[selected=true]:ml-4",
+        ],
+        endContent: "text-tiny",
+        startContent: "text-tiny",
+        label: "text-small",
+      },
+      md: {
+        wrapper: "w-12 h-7 mr-2",
+        thumb: [
+          "w-5 h-5 text-small",
+          //selected
+          "group-data-[selected=true]:ml-5",
+        ],
+        endContent: "text-small",
+        startContent: "text-small",
+        label: "text-medium",
+      },
+      lg: {
+        wrapper: "w-14 h-8 mr-2",
+        thumb: [
+          "w-6 h-6 text-medium",
+          //selected
+          "group-data-[selected=true]:ml-6",
+        ],
+        endContent: "text-medium",
+        startContent: "text-medium",
+        label: "text-large",
+      },
     },
   },
   defaultVariants: {
     color: "default",
     disableAnimation: "false",
   },
+  compoundVariants: [
+    {
+      disableAnimation: false,
+      size: "xs",
+      class: {
+        thumb: [
+          "group-data-[pressed=true]:w-4",
+          "group-data-[selected]:group-data-[pressed]:ml-2",
+        ],
+      },
+    },
+    {
+      disableAnimation: false,
+      size: "sm",
+      class: {
+        thumb: [
+          "group-data-[pressed=true]:w-5",
+          "group-data-[selected]:group-data-[pressed]:ml-3",
+        ],
+      },
+    },
+    {
+      disableAnimation: false,
+      size: "md",
+      class: {
+        thumb: [
+          "group-data-[pressed=true]:w-6",
+          "group-data-[selected]:group-data-[pressed]:ml-4",
+        ],
+      },
+    },
+    {
+      disableAnimation: false,
+      size: "lg",
+      class: {
+        thumb: [
+          "group-data-[pressed=true]:w-7",
+          "group-data-[selected]:group-data-[pressed]:ml-5",
+        ],
+      },
+    },
+  ],
 });
 
-const Switch = ({ id, checked, onChange }: SwitchProps) => {
+const Switch = ({
+  id,
+  size,
+  checked,
+  showIcon,
+  onChange,
+  ...props
+}: SwitchProps) => {
   return (
     <div className="flex w-8">
       <MySwitch
@@ -75,14 +222,21 @@ const Switch = ({ id, checked, onChange }: SwitchProps) => {
         name={id}
         aria-labelledby={`switch_${id}`}
         onChange={onChange}
-        size="xs"
+        size={size ?? "xs"}
         color="default"
         isSelected={checked}
         disableAnimation={false}
-        classNames={{
-          thumb:
-            "z-0 group-data-[pressed=true]:w-4 group-data-[selected]:group-data-[pressed]:ml-2",
+        thumbIcon={({ isSelected }) => {
+          if (showIcon) {
+            return isSelected ? <MoonIcon /> : <SunIcon />;
+          }
         }}
+        {...props}
+        // classNames={{
+        //   thumb:
+        //     size === "xs" &&
+        //     "z-0 group-data-[pressed=true]:w-4 group-data-[selected]:group-data-[pressed]:ml-2",
+        // }}
       />
     </div>
   );
