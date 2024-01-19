5e39f53889f7d1b131059c8c1a79936745177c41
Christopher Chueng <christopherchueng@gmail.com>
2024-01-18 21:01:15 -0500

    style: Add dark mode to snooze modal

--- src/app/_components/UI/Button.tsx
+++ src/app/_components/UI/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({ children, color = "primary", ...props }: ButtonProps) => {
     {
       "border-slate-900 bg-slate-900 hover:bg-slate-700 active:bg-slate-800 dark:bg-white/80 dark:border-white/80 dark:text-slate-900 dark:hover:border-white dark:hover:bg-white dark:active:border-white/80 dark:active:bg-white/80":
         color === "primary",
-      "border-gray-400 bg-gray-400 hover:opacity-80 active:opacity-90":
+      "border-gray-400 bg-gray-400 hover:opacity-80 active:opacity-90 dark:bg-zinc-600 dark:border-zinc-700 dark:text-white/70":
         color === "default",
     },
   );
