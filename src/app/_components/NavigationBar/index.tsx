d8d941063d683cb1f66f5100337d320dd328a94a
Christopher Chueng <christopherchueng@gmail.com>
2024-01-20 15:53:16 -0500

    Only show hamburger menu on dashboard

--- src/app/_components/NavigationBar/index.tsx
+++ src/app/_components/NavigationBar/index.tsx
@@ -4,15 +4,17 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Items from "./Items";
 import { HamburgerMenu, Logo, NameLogo } from "../UI";
+import { usePathname } from "next/navigation";
 
 const NavigationBar = () => {
   const { data: session, status } = useSession();
+  const pathname = usePathname();
 
   return (
     <nav className="absolute z-10 flex w-full items-center justify-center px-6 py-4 text-xs sm:px-8">
       <div className="w-full">
         <div className="flex gap-4">
-          {session && <HamburgerMenu />}
+          {session && pathname === "/dashboard" && <HamburgerMenu />}
           <div className="hidden w-fit sm:block">
             <Link href={status === "authenticated" ? "/dashboard" : "/"}>
               <NameLogo className="h-8 w-24" />
