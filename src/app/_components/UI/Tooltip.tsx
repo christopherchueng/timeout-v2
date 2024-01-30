e9c19ff1412071febd79cdbfd4e2512b13e464a5
Christopher Chueng <christopherchueng@gmail.com>
2024-01-30 12:31:33 -0500

    Center bottom tooltip some more

--- src/app/_components/UI/Tooltip.tsx
+++ src/app/_components/UI/Tooltip.tsx
@@ -17,7 +17,7 @@ const Tooltip = ({
 }: TooltipProps) => {
   const classNames = clsx(
     "absolute w-fit mx-auto z-50 dark:bg-zinc-700 dark:border-zinc-600 dark:text-white/70 animate-dilate whitespace-nowrap rounded border px-2 py-1.5 text-xs transition duration-75",
-    placement === "bottom" && "inset-x-0 -bottom-9 -left-4",
+    placement === "bottom" && "inset-x-0 -bottom-9 -left-3.5",
     placement === "right" && "inset-y-0 left-4 my-auto h-fit",
     color === "default" && "bg-gray-50",
     color === "primary" && "bg-white",
